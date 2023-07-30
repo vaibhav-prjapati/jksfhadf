f6da0690e48c6aeb3b45b3846340b83adc0cebc3
Vaibhav kumar <129750319+vaibhav-prjapati@users.noreply.github.com>
2023-07-30 12:00:41 +0530

    Add files via upload

---
+++ arraybasic.js
@@ -0,0 +1,12 @@
+let a = [1 ,2 ,3,4 , "sanket" , false]
+console.log(a)
+
+let b = new Array("abc" , "def" , "ghi")
+console.log(b)
+
+// given array - > [11,12,,23,45,54]
+//indexes/pos ->   [0, 1,2,3,4 ] 
+//last element index = arr.length - 1
+console.log(a[4])
+a[4] = 5;
+console.log(a)
